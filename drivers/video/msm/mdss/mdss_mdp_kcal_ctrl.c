flar2/ElementalX-N5

mdss: LiveDisplay to kcal interface
Why not just add pcc color interface from cm?  Because I want kcal and pcc to be in sync.  This adds the sysfs node for LiveDisplay but uses kcal to apply and store the values.

drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -27,19 +27,6 @@
 #define DEF_PA 0xff
 #define PCC_ADJ 0x80
 
-struct kcal_lut_data {
-    int red;
-    int green;
-    int blue;
-    int minimum;
-    int enable;
-    int invert;
-    int sat;
-    int hue;
-    int val;
-    int cont;
-};
-
 static uint32_t igc_Table_Inverted[IGC_LUT_ENTRIES] = {
 	267390960, 266342368, 265293776, 264245184,
 	263196592, 262148000, 261099408, 260050816,
@@ -197,6 +184,28 @@ static void kcal_apply_values(struct kcal_lut_data *lut_data)
 		lut_data->minimum : lut_data->blue;
 
 	mdss_mdp_kcal_update_pcc(lut_data);
+}
+
+static struct platform_device kcal_ctrl_device;
+
+void kcal_ext_apply_values(int red, int green, int blue)
+{
+	struct kcal_lut_data *lut_data =
+				platform_get_drvdata(&kcal_ctrl_device);
+
+	lut_data->red = red / 128;
+	lut_data->green = green / 128;
+	lut_data->blue = blue / 128;
+
+	kcal_apply_values(lut_data);
+}
+
+struct kcal_lut_data kcal_ext_show_values(void)
+{
+	struct kcal_lut_data *lut_data =
+				platform_get_drvdata(&kcal_ctrl_device);
+
+	return *lut_data;
 }
 
 static ssize_t kcal_store(struct device *dev, struct device_attribute *attr,

aviraxp/racer-n5x

drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -35,19 +35,6 @@
 
 static int kcal_fix_yellow = 1;
 
-struct kcal_lut_data {
-	int red;
-	int green;
-	int blue;
-	int minimum;
-	int enable;
-	int invert;
-	int sat;
-	int hue;
-	int val;
-	int cont;
-};
-
 static int mdss_mdp_kcal_display_commit(void)
 {
 	int i;
@@ -167,6 +154,29 @@ static void mdss_mdp_kcal_update_pa(struct kcal_lut_data *lut_data)
 
 		mdss_mdp_pa_v2_config(&pa_v2_config, &copyback);
 	}
+}
+
+static struct platform_device kcal_ctrl_device;
+
+void kcal_ext_apply_values(int red, int green, int blue)
+{
+	struct kcal_lut_data *lut_data =
+				platform_get_drvdata(&kcal_ctrl_device);
+
+	lut_data->red = red / 128;
+	lut_data->green = green / 128;
+	lut_data->blue = blue / 128;
+
+	mdss_mdp_kcal_update_pcc(lut_data);
+	mdss_mdp_kcal_display_commit();
+}
+
+struct kcal_lut_data kcal_ext_show_values(void)
+{
+	struct kcal_lut_data *lut_data =
+				platform_get_drvdata(&kcal_ctrl_device);
+
+	return *lut_data;
 }
 
 static ssize_t kcal_store(struct device *dev, struct device_attribute *attr,